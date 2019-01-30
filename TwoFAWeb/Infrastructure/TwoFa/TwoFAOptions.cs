19409b34d5782cb11d5ab726d19cb92bc95579bb
Александр <pirmosk@gmail.com>
2019-01-30 13:40:09 -0800

    .net core 2.2.

---
+++ TwoFAWeb/Infrastructure/TwoFa/TwoFAOptions.cs
@@ -0,0 +1,10 @@
+
+namespace Infrastructure.TwoFa
+{
+    public class SMSoptions
+    {
+        public string SMSAccountIdentification { get; set; }
+        public string SMSAccountPassword { get; set; }
+        public string SMSAccountFrom { get; set; }
+    }
+}
